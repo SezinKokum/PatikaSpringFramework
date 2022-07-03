1202673c269c1728820246f22ccbde58ee8beba7
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-07-03 22:17:20 +0300

    @Named structure

---
+++ src/main/java/com/sezinmutludogan/tutorials/JavaTutorials.java
@@ -0,0 +1,21 @@
+package com.sezinmutludogan.tutorials;
+
+public class JavaTutorials {
+
+    public static void main(String args[]){
+        //Arrays.sort();
+        //this.
+        //super
+        //optional,isPresent java 8 ile
+        //cast
+        //primitive type
+        //wrapper class
+        //random, math
+        //instanceOf bir nesne o sınıfa ait mi
+        //public private protected
+        //String StringBuilder StringTokenizer StringBuffer
+        //Enum multithread SOLID
+
+        //Java 8, stream, ap,filter, sort, optional, lambda expression,method references
+    }
+}
